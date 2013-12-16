c98d7be7834e0a879f16c032b85b0336169a564c
yixinglu <yxlyoo@gmail.com>
2013-12-16 21:43:34 +0800

    modify test

--- source/Test/TestVector3.mk
+++
@@ -1,5 +0,0 @@
-vpath % ../Mesh/
-TestVector3 :  TestVector3.o
-	g++ -o TestVector3 TestVector3.o
-TestVector3.o : TestVector3.cpp Vector3.h
-	g++ -c TestVector3.cpp
--- source/Test/TestVertex.mk
+++
@@ -1,8 +0,0 @@
-vpath % ../Mesh/
-#VPATH = ../Mesh/
-TestVertex : TestVertex.o Vertex.o
-	g++ -o TestVertex TestVertex.o Vertex.o
-TestVertex.o : TestVertex.cpp Vertex.h
-	g++ -c TestVertex.cpp
-Vertex.o : Vertex.cpp Vertex.h Point3.h Vector3.h
-	g++ -c ../Mesh/Vertex.cpp
